2e8c384cff63e08f82787cab149d7de49aad7187
Kumar Vicku <84949946+kumarvicku@users.noreply.github.com>
2022-02-22 18:57:47 +0530

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+    setInterval(updateTime, 1000)
+};
+
+function updateTime () {
+    const hourEL = document.querySelector("#hour");
+    const minuteEL = document.querySelector("#minute");
+    const secondEL = document.querySelector("#second");
+    let d = new Date();
+    let hours = d.getHours();
+    let minutes =d.getMinutes();
+    let seconds = d.getSeconds();
+    if (hours >=0 && hours <=9) hours = "0" + hours; 
+    
+    
+    if (minutes >=0 && minutes <=9) minutes = "0" + minutes; 
+    
+    
+    if (seconds >=0 && seconds <=9) seconds = "0" + seconds;
+    hourEL.innerHTML = hours;
+    minuteEL.innerHTML = minutes;
+    secondEL.innerHTML = seconds;
+
+    const dateEl = document.querySelector("#date");
+    let date = d.getDate();
+    if (date >=0 && date <=9) date = "0" + date;
+    let month = d.getMonth() +1;
+    if (month >=0 && month <=9) month= "0" + month;
+    let year = d.getFullYear();
+    if (year >=0 && year <=9) year = "0" + year;
+    dateEl.innerHTML = `${date}/${month}/${year}`;
+    
+    const dayEL = document.querySelector("#day");
+    let daynumber = d.getDay(), day = "";
+    switch (daynumber) {
+        case 0: day = "SUNDAY"; break;
+        case 1: day = "MONDAY"; break;
+        case 2: day = "TUESDAY"; break;
+        case 3: day = "WEDNESDAY"; break;
+        case 4: day = "THURSDAY"; break;
+        case 5: day = "FRIDAY"; break;
+        case 6: day = "SATURDAY"; break;
+    }
+    dayEL.innerHTML = day;
+}
